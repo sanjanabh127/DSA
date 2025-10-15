26b61deb87f621177adb97923a490f4373056311
YO <sanjanabh127@gmail.com>
2025-10-15 19:53:00 +0530

    Time: 29 ms (41.43%) | Memory: 48.5 MB (27.01%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -1,30 +1,35 @@
-
-
 class Solution {
-    private int sumD(int[] arr, int div) {
-        int sum = 0;
-        int n = arr.length;
-        for (int i = 0; i < n; i++) {
-            sum += Math.ceil((double) arr[i] / (double) div);
+    private int sumD(int[] arr,int divisor){
+        int n=arr.length;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum=sum+(int)Math.ceil((double)arr[i] / divisor);
         }
         return sum;
     }
-
     public int smallestDivisor(int[] nums, int threshold) {
-        int ans = -1;
-        int low = 1;
-        int high = Arrays.stream(nums).max().getAsInt();
-
-        while (low <= high) {
-            int mid = (low + high) / 2;  
-
-            if (sumD(nums, mid) <= threshold) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        int n=nums.length;
+        
+        if(n>threshold)return -1;
+        int low=1;
+        int high = nums[0];
+       for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > high) {
+                  high = nums[i];
+                }
+       }
+        int ans = -1; 
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(sumD(nums,mid)<=threshold){
+                 ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
         return ans;
+        
     }
 }
